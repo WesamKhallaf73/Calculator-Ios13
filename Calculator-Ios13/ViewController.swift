476006c6804d1648f04daa154861ba5fe574eefb
wesamKhallaf <wesamkhallaf@gmail.com>
2020-04-23 11:13:24 +0200

    Initial Commit

---
+++ Calculator-Ios13/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator-Ios13
+//
+//  Created by wesam Khallaf on 4/23/20.
+//  Copyright © 2020 wesam Khallaf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
